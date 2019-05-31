254e565e37fb2d8841480582c8dc6618bbb733f5
Tim Mollenhauer <stingwing@gmail.com>
2019-05-31 21:19:42 +1000

    Test

---
+++ magicCounter/magicCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magicCounter
+//
+//  Created by Tim Mollenhauer on 31/5/19.
+//  Copyright © 2019 Tim Mollenhauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

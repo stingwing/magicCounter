afb4031512cf78ebb9e8b3a60afeff42790a28d3
Tim Mollenhauer <stingwing@gmail.com>
2019-05-31 21:29:06 +1000

    created initial label, buttons and variables for app

--- magicCounter/magicCounter/ViewController.swift
+++ magicCounter/magicCounter/ViewController.swift
@@ -13,8 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        output.text = String(current)
     }
-
-
+    
+    let initial = 20
+    var current = 20
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        current = current + 1
+        print(current)
+        viewDidLoad()
+        
+    }
+    @IBAction func minus(_ sender: Any) {
+        current = current - 1
+        print(current)
+        viewDidLoad()
+    }
+    
+    
+    @IBOutlet weak var output: UILabel!
 }
 
